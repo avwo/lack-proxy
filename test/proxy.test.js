a74531da3432c10803d580eae09743e20e76ad60
avwo <avwu@qq.com>
2020-10-23 10:21:01 +0800

    feat: http2

--- test/proxy.test.js
+++ test/proxy.test.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const https = require('https');
-
+const http2 = require('http2');
 const { setProxy } = require('../');
 
 const PROXY_OPTIONS = {
@@ -14,6 +14,24 @@ const PROXY_OPTIONS = {
 
 // 设置固定代理
 setProxy(PROXY_OPTIONS);
+const client = http2.connect('https://ke.qq.com:443');
+client.on('error', (err) => console.error(err)); // eslint-disable-line
+
+const req = client.request({ ':path': '/' });
+
+req.on('response', (headers) => {
+  Object.keys(headers).forEach(name =>  console.log(`${name}: ${headers[name]}`)); // eslint-disable-line
+});
+
+req.setEncoding('utf8');
+let data = '';
+req.on('data', (chunk) => { data += chunk; });
+req.on('end', () => {
+  console.log(`\n${data}`); // eslint-disable-line
+  client.close();
+});
+req.end();
+
 
 const httpClient = http.request('http://ke.qq.com', (res) => {
   console.log(res.statusCode); // eslint-disable-line
@@ -22,7 +40,7 @@ httpClient.on('error', console.error); // eslint-disable-line
 httpClient.end();
 
 const httpsClient = https.request('https://ke.qq.com', (res) => {
-  console.log(res.statusCode); // eslint-disable-line
+  // console.log(res.statusCode); // eslint-disable-line
 });
 httpsClient.on('error', console.error); // eslint-disable-line
 httpsClient.end();

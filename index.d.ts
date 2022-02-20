c2e97d317026ed23d382e83737b1efc5b005acc9
avwo <avwu@qq.com>
2022-02-20 20:12:46 +0800

    Release v1.3.6

--- index.d.ts
+++ index.d.ts
@@ -1,11 +1,11 @@
 /// <reference types="node" />
 
-export type FilterRequest = (opts?: object, isSocket?: boolean) => boolean;
+export type FilterRequest = (opts?: any, isSocket?: boolean) => boolean;
 
 interface ProxyOptions {
   host: string;
   port: number;
-  headers?: object;
+  headers?: any;
   servername?: string;
   filterRequest?: FilterRequest;
   allowlist?: string | string[];
@@ -13,13 +13,13 @@ interface ProxyOptions {
   socket?: false | {
     host: string;
     port: number;
-    headers?: object;
+    headers?: any;
   };
 }
 
 type SetProxy = (options: Options) => Result;
 
-export type ProxyFilter = (options: object, isHttps?: boolean) => ProxyOptions;
+export type ProxyFilter = (options: any, isHttps?: boolean) => ProxyOptions;
 
 export type Result = ProxyFilter | ProxyOptions;
 
@@ -33,11 +33,11 @@ export type ReqOpts = string | {
 export const socket: {
   setUrl(opts: ReqOpts, url: string): ReqOpts;
   getHeaders(): any;
-  setHeaders(opts: ReqOpts, headers: object): ReqOpts;
+  setHeaders(opts: ReqOpts, headers: any): ReqOpts;
   removeHeaders(opts: ReqOpts): void;
 };
 
-export const getProxy: (opts?: object, isHttps?: boolean) => Result;
+export const getProxy: (opts?: any, isHttps?: boolean) => Result;
 export const setProxy: SetProxy;
 export const proxy: SetProxy;
 export const removeProxy: () => void;

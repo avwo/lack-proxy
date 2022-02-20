df8886642f5d1f0be4906ea2d0d30de8fa1a5d3a
avwo <avwu@qq.com>
2022-02-20 19:54:53 +0800

    refactor: refine types

--- index.d.ts
+++ index.d.ts
@@ -2,19 +2,19 @@
 
 export type FilterRequest = (opts?: object, isSocket?: boolean) => boolean;
 
-type ProxyOptions = {
-  host: string,
-  port: number,
-  headers?: object,
-  servername?: string,
-  filterRequest?: FilterRequest,
-  allowlist?: string | string[],
-  blocklist?: string | string[],
+interface ProxyOptions {
+  host: string;
+  port: number;
+  headers?: object;
+  servername?: string;
+  filterRequest?: FilterRequest;
+  allowlist?: string | string[];
+  blocklist?: string | string[];
   socket?: false | {
-    host: string,
-    port: number,
-    headers?: object,
-  },
+    host: string;
+    port: number;
+    headers?: object;
+  };
 };
 
 type SetProxy = (options: Options) => Result;
@@ -26,15 +26,15 @@ export type Result = ProxyFilter | ProxyOptions;
 export type Options = ProxyOptions | ProxyFilter;
 
 export type ReqOpts = string | {
-  host: string,
-  [propName: string]: any,
+  host: string;
+  [propName: string]: any;
 };
 
 export const socket: {
-  setUrl: (opts: ReqOpts, url: string) => ReqOpts,
-  getHeaders: () => Object,
-  setHeaders: (opts: ReqOpts, headers: object) => ReqOpts,
-  removeHeaders: (opts: ReqOpts) => void,
+  setUrl(opts: ReqOpts, url: string): ReqOpts;
+  getHeaders(): Object;
+  setHeaders(opts: ReqOpts, headers: object): ReqOpts;
+  removeHeaders(opts: ReqOpts): void;
 };
 
 export const getProxy: (opts?: object, isHttps?: boolean) => Result;
